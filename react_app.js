7a51ee547d30caae78e9330a05e7f582bf89d900
GatesKennedy <conor@esccollective.com>
2019-07-27 16:54:51 -0700

    NEW: first react application: 'react-app'

---
+++ react_app.js
@@ -0,0 +1,31 @@
+/* 
+Start New App (Zero-Config Setup)
+    CMD> create-react-app react-app
+        [^method^]       [^name^]
+
+        Creates new Configured application 
+            New folder named 'react-app'
+            installs react
+            installs 3rd party libraries
+                Development server (light weight)
+                Webpack (bundling files)
+                Babel   (compile js code)
+Eject App
+    Edit Configuration for Production Env
+    CMD> npm run eject
+
+Run New App
+    Navigate to 'react-app' folder
+    CMD> npm start
+        Launches development server
+            localhost:3000 (default)
+        Opens default browser
+
+
+
+
+
+
+*/
+
+    
