2d4cf1369bdc9c2f424f9615272d1e5868ba33dd
GatesKennedy <conor@esccollective.com>
2019-07-27 16:17:11 -0700

    "Initial Commit" Initial Commit

---
+++ Mosh_Overview.js
@@ -0,0 +1,28 @@
+/*
+REACT
+    JavaScript Library
+    Components
+        Structure Tree
+            Root Component
+                Child Components
+        Implementation
+            JS Class
+                state
+                render method
+                    Returns : React Elements
+            Virtual DOM = JS Object that "maps with" DOM Element
+                Light weight representation of the DOM stored in memory
+                State Changes are compared to Real DOM and updates
+    NodeJS
+        Latest ver
+            $npm i -g create-react-app
+        Specific ver
+            $npm i -g create-react-app@1.5.2
+
+*/
+class Tweet {
+    state = {/*...*/};
+    render() {
+        // Returns : React Element
+    }
+}
